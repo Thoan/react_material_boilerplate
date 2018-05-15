dcc6dc73b76cdc7e77e06bbd676ce1238a25f157
Thoan <thoan.thai90@gmail.com>
2018-05-14 20:46:27 -0700

    boilerplate

--- app.js
+++ app.js
@@ -5,17 +5,18 @@ export default class App extends Component {
 
   // Arrow function
   foo() {
-    console.log("arrow functions!")
+    console.log("arrow functions!");
   }
 
   render() {
     return (
       <div style={ {} }>
-        <button onClick={() => this.foo()} className="foo-button mdc-button">
-          Button
-        </button>
-
-        <Button raised>
+        <button className="foo-button mdc-button">Button</button>
+        <Button
+          raised
+          onClick={() => this.foo()}
+          className="foo-button"
+        >
           BBBBBUTTON
         </Button>
       </div>
